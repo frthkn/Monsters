95b019684158d89e7b7ce8a03b78cacc5cb8781a
frthkn <hknfrt@protonmail.com>
2023-04-22 15:43:44 +0300

    add new useEffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,17 +9,26 @@ const App = () => {
 
     const [searchField, setSearchField] = useState('');
     const [monsters, setMonsters] = useState([])
-    console.log({ searchField })
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters)
+   
     const onSearchChange = (event) => {
         const searchFieldString = event.target.value.toLocaleLowerCase();
         setSearchField(searchFieldString)
 
     }
 
-    const filteredMonsters = monsters.filter((monster) => {
-        return monster.name.toLocaleLowerCase().includes(searchField)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => setMonsters(users))
+    }, [])
 
+   useEffect(()=>{
+    const newFilteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLocaleLowerCase().includes(searchField)
     });
+    setFilteredMonsters(newFilteredMonsters)
+   },[monsters, searchField])
 
     return (
         <div className="App">
